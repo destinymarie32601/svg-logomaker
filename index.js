9fcd0efd52eeb800cef68bd719ea9b5f25a1da8d
destiny32601 <destiny32601@gmail.com>
2023-07-07 12:47:28 -0400

    finished commenting code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const inquirer = require("inquirer");
+const inquirer = require("inquirer"); //install required packages
 const fs = require("fs");
 const generateSvg = require("./lib/logogenerator");
-const path = require("path");
+const path = require("path"); //requires path to send the files to the example folder
 
 const textLength = async (input) => {
     if (input.Length > 3 || input.Length < 1) {
         console.log("You must enter the correct amount of characters");
     } else {
-        return true;
+        return true;  //validating the text input
     }
 }
 
-const userPrompt = inquirer.prompt([
+const userPrompt = inquirer.prompt([ //prompt for the user input to design their logo
     {
         type: 'input',
         name: 'text',
@@ -34,7 +34,7 @@ const userPrompt = inquirer.prompt([
         name:'shapeColor',
         message:'What color would you like your logo to be?',
     }
-]) .then(response => {
+]) .then(response => {  //sends the users shape with the _logo.svg file name to the examples folder
     const logo = generateSvg(response);
     const filename = `${response.shape.toLowerCase()}_logo.svg`;
     const filePath = path.join(__dirname, 'examples', filename);
@@ -42,9 +42,9 @@ const userPrompt = inquirer.prompt([
     fs.writeFile(filePath, logo, (err) => {
         if (err) {
             console.error('Error creating the logo', err);
-        } else {
+        } else { //console log error if any errors
             console.log(`Generated ${filename} in the examples folder`);
-        }
+        } //console log if the logo generator was successful
     });
 })
 .catch(error => {
